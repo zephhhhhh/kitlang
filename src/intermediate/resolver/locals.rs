bb12aa9059282b32be1bd3e9053e910baef89dc7
zephhhhhh <54158891+zephhhhhh@users.noreply.github.com>
2025-12-11 07:56:24 +0000

    Separated resolver into a directory module with files corresponding to each stage/type of resolution.

---
+++ src/intermediate/resolver/locals.rs
@@ -0,0 +1,185 @@
+use ::std::fmt::Debug;
+use std::collections::HashMap;
+
+use crate::intermediate::hir::errors::{LowerResult, LoweringError, LoweringErrorKind};
+use crate::intermediate::hir::nodes::{
+    Block, Function, LetStatement, Parameter, RefPath, ResolvedID,
+};
+use crate::intermediate::hir::visitor::{HLIRDisjointMut, HLIRVisitorMut};
+use crate::intermediate::hir::{HLIR, HirId};
+
+#[derive(Debug, Clone, PartialEq)]
+struct LocalScope {
+    pub scope_ident: Option<String>,
+    pub definitions: HashMap<String, ResolvedID>,
+    pub parent: Option<Box<Self>>,
+}
+
+impl LocalScope {
+    pub fn new(scope_ident: Option<String>) -> Self {
+        Self::new_with_parent(scope_ident, None)
+    }
+
+    pub fn new_with_parent(scope_ident: Option<String>, parent: Option<Box<Self>>) -> Self {
+        Self {
+            scope_ident,
+            definitions: HashMap::new(),
+            parent,
+        }
+    }
+
+    pub fn child_scope(&self, scope_ident: Option<String>) -> Self {
+        Self::new_with_parent(scope_ident, Some(Box::new(self.clone())))
+    }
+
+    #[allow(dead_code)]
+    pub fn is_root_scope(&self) -> bool {
+        self.parent.is_none()
+    }
+
+    pub fn add_definition_unique(&mut self, name: &str, id: ResolvedID) -> bool {
+        if self.definitions.contains_key(name) {
+            return false;
+        }
+
+        self.definitions.insert(name.to_string(), id).is_none()
+    }
+
+    /// Add a new definition, redefining the value that was already there, if exists.
+    pub fn add_definition_overwrite(&mut self, name: &str, id: ResolvedID) -> bool {
+        self.definitions.insert(name.to_string(), id).is_some()
+    }
+
+    #[allow(dead_code)]
+    pub fn add_definition_result(&mut self, name: &str, id: ResolvedID) -> LowerResult<()> {
+        if self.add_definition_unique(name, id) {
+            Ok(())
+        } else {
+            Err(LoweringError::new(
+                LoweringErrorKind::VariableAlreadyDefined(name.to_string()),
+            ))
+        }
+    }
+
+    pub fn find_definition(&self, name: &str) -> Option<ResolvedID> {
+        if let Some(id) = self.definitions.get(name) {
+            Some(*id)
+        } else if let Some(parent) = &self.parent {
+            parent.find_definition(name)
+        } else {
+            None
+        }
+    }
+}
+
+struct ScopeResolver {
+    pub scope: Vec<LocalScope>,
+    pub errors: Vec<LoweringError>,
+}
+
+impl ScopeResolver {
+    pub fn new() -> Self {
+        Self {
+            scope: Vec::new(),
+            errors: Vec::new(),
+        }
+    }
+
+    pub fn push_scope(&mut self, local_scope: LocalScope) {
+        self.scope.push(local_scope);
+    }
+
+    pub fn pop_scope(&mut self) {
+        self.scope.pop();
+    }
+
+    pub fn current_scope(&self) -> &LocalScope {
+        self.scope.last().expect("Must have valid scope.")
+    }
+
+    pub fn current_scope_mut(&mut self) -> &mut LocalScope {
+        self.scope.last_mut().expect("Must have valid scope.")
+    }
+
+    pub fn push_child_scope(&mut self, scope_ident: Option<String>) {
+        self.push_scope(self.current_scope().child_scope(scope_ident))
+    }
+
+    pub fn resolve(&mut self, hlir: &mut HLIR) -> LowerResult<()> {
+        self.walk_mut(hlir);
+
+        if self.errors.is_empty() {
+            Ok(())
+        } else {
+            // FIXME: This is not good dx.
+            Err(self.errors.first().expect("There is an error.").clone())
+        }
+    }
+}
+
+impl HLIRVisitorMut<'_> for ScopeResolver {
+    fn visit_block_mut(&mut self, block: &mut Block, hlir: &mut HLIRDisjointMut<'_>) {
+        if block.root_block {
+            self.push_child_scope(None);
+            self.super_block_mut(block, hlir);
+            self.pop_scope();
+        } else {
+            self.super_block_mut(block, hlir);
+        }
+    }
+
+    fn visit_function_param_mut(
+        &mut self,
+        parameter: &mut Parameter,
+        _hlir: &mut HLIRDisjointMut<'_>,
+    ) {
+        if let Err(e) = self
+            .current_scope_mut()
+            .add_definition_result(parameter.ident.str(), parameter.id.into())
+        {
+            self.errors.push(e);
+        }
+    }
+
+    fn visit_function_mut(&mut self, function: &mut Function, hlir: &mut HLIRDisjointMut<'_>) {
+        let has_body = function.body.is_some();
+        if has_body {
+            self.push_scope(LocalScope::new(Some(function.ident.string())));
+        }
+        self.super_function_mut(function, hlir);
+
+        if has_body {
+            self.pop_scope();
+        }
+    }
+
+    fn visit_let_statement_mut(
+        &mut self,
+        id: HirId,
+        let_statement: &mut LetStatement,
+        hlir: &mut HLIRDisjointMut<'_>,
+    ) {
+        self.current_scope_mut()
+            .add_definition_overwrite(let_statement.ident.str(), id.into());
+
+        self.super_let_statement_mut(id, let_statement, hlir);
+    }
+
+    fn visit_path_mut(
+        &mut self,
+        _hir_id: HirId,
+        path: &mut RefPath,
+        _hlir: &mut HLIRDisjointMut<'_>,
+    ) {
+        if !path.is_resolved() {
+            let ident_to_find = path.ident_path().to_string();
+            if let Some(id) = self.current_scope().find_definition(&ident_to_find) {
+                path.resolve_to(id);
+            }
+        }
+    }
+}
+
+pub fn resolve_scope_paths(hlir: &mut HLIR) -> LowerResult<()> {
+    ScopeResolver::new().resolve(hlir)
+}
---
+++ src/intermediate/resolver/types.rs
@@ -0,0 +1,231 @@
+use crate::ast::IdentPath;
+
+use crate::intermediate::hir::errors::{LowerResult, LoweringError};
+use crate::intermediate::hir::nodes::{
+    Expr, ExprKind, Function, HIRNode, Impl, Module, RefPath, ResolvedID, Struct, Type,
+};
+use crate::intermediate::hir::visitor::{HLIRDisjointMut, HLIRVisitorMut};
+use crate::intermediate::hir::{HLIR, OwnerDefId};
+
+use crate::intermediate::resolver::{Namespace, NamespaceKind, TypeID, TypeRegistry};
+use crate::intermediate::types::KitTy;
+
+use log::*;
+
+struct TypeResolver<'a> {
+    pub current_impl: Option<Type>,
+
+    pub path_stack: Vec<IdentPath>,
+    pub type_registry: TypeRegistry,
+
+    pub root_namespace: &'a Namespace,
+
+    // TODO: Implement these errors!!
+    pub errors: Vec<LoweringError>,
+}
+
+impl<'a> TypeResolver<'a> {
+    pub fn new(root_namespace: &'a Namespace, registry: TypeRegistry) -> Self {
+        Self {
+            current_impl: None,
+            path_stack: vec![IdentPath::from_segments(Vec::new(), true)],
+            type_registry: registry,
+            root_namespace,
+            errors: Vec::new(),
+        }
+    }
+}
+
+impl TypeResolver<'_> {
+    pub fn resolve_types(&mut self, hlir: &mut HLIR) -> LowerResult<TypeRegistry> {
+        self.walk_mut(hlir);
+
+        Ok(self.type_registry.clone())
+    }
+
+    fn resolve_type(&self, path: &IdentPath) -> Option<TypeID> {
+        let final_path = path.rebase_from_path(
+            &self
+                .root_namespace
+                .find_previous_module(self.current_path()),
+        );
+
+        let def = self.root_namespace.find_definition(&final_path)?;
+        if let NamespaceKind::Struct(ty_id) = def.kind {
+            Some(ty_id)
+        } else {
+            error!(
+                "Type path is not a struct: {path:?}, but instead: {:?}",
+                def.kind
+            );
+            None
+        }
+        //self.type_registry.find_type_id_from_path(&type_path)
+    }
+}
+
+impl TypeResolver<'_> {
+    pub fn current_path(&self) -> &IdentPath {
+        self.path_stack
+            .last()
+            .expect("Always has atleast 1 in stack.")
+    }
+
+    pub fn current_path_mut(&mut self) -> &mut IdentPath {
+        self.path_stack
+            .last_mut()
+            .expect("Always has atleast 1 in stack.")
+    }
+
+    pub fn push_to_current_path(&mut self, s: &str) {
+        self.current_path_mut().push(s);
+    }
+
+    pub fn pop_from_current_path(&mut self) {
+        self.current_path_mut().pop();
+    }
+
+    #[allow(dead_code)]
+    pub fn push_stack(&mut self, path: IdentPath) {
+        self.path_stack.push(path);
+    }
+
+    #[allow(dead_code)]
+    pub fn pop_stack(&mut self) {
+        self.path_stack.pop();
+    }
+}
+
+impl HLIRVisitorMut<'_> for TypeResolver<'_> {
+    fn visit_expr_mut(&mut self, expr: &mut Expr, hlir: &mut HLIRDisjointMut<'_>) {
+        match &mut expr.kind {
+            ExprKind::StructInit(struct_init) => {
+                if let RefPath::Unresolved(ty_path) = &struct_init.ty_path {
+                    if let Some(type_id) = self.resolve_type(&ty_path.path) {
+                        struct_init.ty_path =
+                            RefPath::Resolved(ty_path.clone(), ResolvedID::TypeDef(type_id));
+                    } else {
+                        error!("Failed to resolve struct init type!");
+                    }
+                }
+            }
+            ExprKind::MethodCall(tar, ident, args) => {}
+            _ => {}
+        }
+
+        self.super_expr_mut(expr, hlir);
+    }
+
+    fn visit_module_mut(&mut self, module: &mut Module, hlir: &mut HLIRDisjointMut<'_>) {
+        if module.owner_id != OwnerDefId::ROOT_NODE {
+            self.push_to_current_path(module.ident.ident().str());
+            self.super_module_mut(module, hlir);
+            self.pop_from_current_path();
+        } else {
+            self.super_module_mut(module, hlir);
+        }
+    }
+
+    fn visit_function_mut(&mut self, function: &mut Function, hlir: &mut HLIRDisjointMut<'_>) {
+        self.push_to_current_path(function.ident.str());
+        self.super_function_mut(function, hlir);
+
+        for arg in &mut function.sig.parameters {
+            match arg {
+                Type::Unresolved(crate::ast::Ty::Type(type_path)) => {
+                    if let Some(type_id) = self.resolve_type(type_path) {
+                        *arg = Type::Resolved(KitTy::Abstract(type_id));
+                    } else {
+                        error!("Failed to resolve function arg type path: {:?}", arg);
+                    }
+                }
+                Type::Unresolved(crate::ast::Ty::This(_s)) => {
+                    if let Some(impl_ty) = &self.current_impl {
+                        *arg = impl_ty.clone();
+                    } else {
+                        error!("Failed to resolve self function arg.");
+                    }
+                }
+                _ => {}
+            }
+            if let Type::Unresolved(crate::ast::Ty::Type(type_path)) = arg {
+                if let Some(type_id) = self.resolve_type(type_path) {
+                    *arg = Type::Resolved(KitTy::Abstract(type_id));
+                } else {
+                    error!("Failed to resolve function arg type path: {:?}", arg);
+                }
+            }
+        }
+
+        if let Type::Unresolved(crate::ast::Ty::Type(type_path)) = &function.sig.output {
+            if let Some(type_id) = self.resolve_type(type_path) {
+                function.sig.output = Type::Resolved(KitTy::Abstract(type_id));
+            } else {
+                error!("Failed to resolve function type path");
+            }
+        }
+
+        self.pop_from_current_path();
+    }
+
+    fn visit_struct_mut(&mut self, structure: &mut Struct, hlir: &mut HLIRDisjointMut<'_>) {
+        self.push_to_current_path(structure.ident.str());
+        let current_struct_path = self.current_path().clone();
+        let Some(current_struct_type_id) = self.resolve_type(&current_struct_path) else {
+            error!("Failed to resolve struct: {:?} type id!", structure.ident);
+            return;
+        };
+        for field_id in &structure.fields {
+            if let Some(HIRNode::Field(field)) = hlir.get_hir_node_mut(*field_id) {
+                let resolved_type =
+                    if let Type::Unresolved(crate::ast::Ty::Type(type_path)) = &field.ty {
+                        self.resolve_type(&type_path.path)
+                    } else {
+                        None
+                    };
+
+                if let Some(type_id) = resolved_type {
+                    field.ty = Type::Resolved(KitTy::Abstract(type_id));
+
+                    let struct_info = self
+                        .type_registry
+                        .get_from_type_id_mut(current_struct_type_id)
+                        .expect("Has to exist.");
+                    if let Some(field_info) = struct_info.get_field_by_ident_mut(field.ident.str())
+                    {
+                        field_info.ty = Type::Resolved(KitTy::Abstract(type_id));
+                    } else {
+                        error!("Failed to find field info: {:?}", field.ident.str());
+                    }
+                }
+            }
+        }
+        self.pop_from_current_path();
+    }
+
+    fn visit_impl_mut(&mut self, impl_info: &mut Impl, hlir: &mut HLIRDisjointMut<'_>) {
+        let impl_path = if impl_info.self_ty.is_root_relative() {
+            impl_info.self_ty.clone()
+        } else {
+            let mut ip = self.current_path().clone();
+            ip.push_path(&impl_info.self_ty);
+            ip
+        };
+
+        if let Some(type_id) = self.type_registry.find_type_id_from_path(&impl_path) {
+            self.current_impl = Some(Type::Resolved(KitTy::Abstract(type_id)));
+            self.super_impl_mut(impl_info, hlir);
+            self.current_impl = None;
+        } else {
+            error!("Failed to get type id for info: {:?}", impl_info.self_ty);
+        }
+    }
+}
+
+pub fn resolve_types(
+    hlir: &mut HLIR,
+    root_namespace: &Namespace,
+    registry: TypeRegistry,
+) -> LowerResult<TypeRegistry> {
+    TypeResolver::new(root_namespace, registry).resolve_types(hlir)
+}
