f0f3ac76289c65a5895b2e44d0ac79648d8559c1
zephhhhhh <zephhhhhh@users.noreply.github.com>
2025-12-26 13:30:09 +0000

    deploy: a80e3d73049a72a7ffe75665fd5ad65c1f1317a2

---
+++ trait.impl/core/convert/trait.AsMut.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["kitlang",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;<a class=\"struct\" href=\"kitlang/intermediate/resolver/struct.Namespace.html\" title=\"struct kitlang::intermediate::resolver::Namespace\">Namespace</a>&gt; for <a class=\"struct\" href=\"kitlang/intermediate/resolver/struct.RootNamespace.html\" title=\"struct kitlang::intermediate::resolver::RootNamespace\">RootNamespace</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[496]}
